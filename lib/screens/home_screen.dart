b28a035f336c5efb563492c4a771a748d42d04f7
Gaby Nieva <gaby.nieva@outlook.com>
2025-04-04 11:05:46 -0600

    Adding images

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,30 +6,57 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
+        body: Column(
+      children: <Widget>[
+        _recipesCard(context),
+        _recipesCard(context),
+      ],
+    ));
+  }
+
+  Widget _recipesCard(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
         width: MediaQuery.of(context).size.width,
         height: 125,
         child: Card(
           child: Row(children: [
-            SizedBox(
+            Container(
               height: 125,
               width: 100,
               child: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Container(),
+                borderRadius: BorderRadius.circular(12),
+                child: Image.network(
+                    'https://static.platzi.com/media/uploads/flutter_lasana_b894f1aee1.jpg',
+                    fit: BoxFit.cover),
               ),
             ),
             const SizedBox(
               width: 26,
             ),
             Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text('Lasagna'),
-                const Text('Gaby N.'),
+                const Text(
+                  'Lasagna',
+                  style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),
+                ),
+                const SizedBox(
+                  height: 4.0,
+                ),
                 Container(
                   height: 2,
                   width: 75,
                   color: Colors.orange,
+                ),
+                const Text(
+                  'Gaby N.',
+                  style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),
+                ),
+                const SizedBox(
+                  height: 4.0,
                 ),
               ],
             ),
