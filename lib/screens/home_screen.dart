b538bb854d067eab7e9104653f35b6b9f8af44f9
Gaby Nieva <gaby.nieva@outlook.com>
2025-05-14 16:06:07 -0600

    Recovering recipes from json using Mockon

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,22 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:recipe_book/screens/recipe_detail.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  Future<List<dynamic>> fetchRecipes() async {
+    final url = Uri.parse('http://localhost:3001/recipes');
+    final response = await http.get(url);
+    final data = jsonDecode(response.body);
+    return data['recipes'];
+  }
+
   @override
   Widget build(BuildContext context) {
+    fetchRecipes();
     return Scaffold(
       body: Column(
         children: <Widget>[
