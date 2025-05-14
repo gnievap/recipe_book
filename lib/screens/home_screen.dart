7186857b2a744faf3fa2c97b1ab344881c02c467
Gaby Nieva <gaby.nieva@outlook.com>
2025-05-14 15:36:59 -0600

    Adding Recipe Detail Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_book/screens/recipe_detail.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -33,52 +34,61 @@ class HomeScreen extends StatelessWidget {
   }
 
   Widget _recipesCard(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: 125,
-        child: Card(
-          child: Row(children: [
-            Container(
-              height: 125,
-              width: 100,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(12),
-                child: Image.network(
-                    'https://static.platzi.com/media/uploads/flutter_lasana_b894f1aee1.jpg',
-                    fit: BoxFit.cover),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    const RecipeDetail(recipeName: 'Lasagna')));
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: 125,
+          child: Card(
+            child: Row(children: [
+              Container(
+                height: 125,
+                width: 100,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.network(
+                      'https://static.platzi.com/media/uploads/flutter_lasana_b894f1aee1.jpg',
+                      fit: BoxFit.cover),
+                ),
               ),
-            ),
-            const SizedBox(
-              width: 26,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Lasagna',
-                  style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),
-                ),
-                const SizedBox(
-                  height: 4.0,
-                ),
-                Container(
-                  height: 2,
-                  width: 75,
-                  color: Colors.orange,
-                ),
-                const Text(
-                  'Gaby N.',
-                  style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),
-                ),
-                const SizedBox(
-                  height: 4.0,
-                ),
-              ],
-            ),
-          ]),
+              const SizedBox(
+                width: 26,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Lasagna',
+                    style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),
+                  ),
+                  const SizedBox(
+                    height: 4.0,
+                  ),
+                  Container(
+                    height: 2,
+                    width: 75,
+                    color: Colors.orange,
+                  ),
+                  const Text(
+                    'Gaby N.',
+                    style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),
+                  ),
+                  const SizedBox(
+                    height: 4.0,
+                  ),
+                ],
+              ),
+            ]),
+          ),
         ),
       ),
     );
